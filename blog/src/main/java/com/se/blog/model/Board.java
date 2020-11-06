1e6e59a8df314e25c7f551206c24217bbbe8e371
seunnnc <seunnnc@users.noreply.github.com>
2020-11-07 02:21:45 +0900

    Board 테이블 생성

--- blog/src/main/java/com/se/blog/model/Board.java
+++ blog/src/main/java/com/se/blog/model/Board.java
@@ -1,15 +1,18 @@
 package com.se.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
@@ -39,9 +42,12 @@ public class Board {
 	@ColumnDefault("0")
 	private int count;	//조회수 
 
-	@ManyToOne	//Many = Board, One = User -> 여러 게시물 한 명의 유저가 쓸 수 있음 
+	@ManyToOne(fetch = FetchType.EAGER)	//Many = Board, One = User -> 여러 게시물 한 명의 유저가 쓸 수 있음 
 	@JoinColumn(name = "userId")
 	private User user;	//DB는 오브젝트 저장할 수 없으니 자바는 저장 가능, FK
+	
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER)	//mappedBy : 연관관걔의 주인이 아니다(FK가 아님) DB에 컬럼 만들지X 
+	private List<Reply> reply;
 	
 	@CreationTimestamp
 	private Timestamp createDate;
