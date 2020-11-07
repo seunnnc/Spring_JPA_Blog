0cb5c841672f739bde4d8b4ef25ce64a7f000aa6
seunnnc <seunnnc@users.noreply.github.com>
2020-11-07 16:27:50 +0900

    paging test

--- blog/src/main/java/com/se/blog/test/DummyControllerTest.java
+++ blog/src/main/java/com/se/blog/test/DummyControllerTest.java
@@ -1,8 +1,14 @@
 package com.se.blog.test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.*;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +23,20 @@ public class DummyControllerTest {
 	
 	@Autowired	//의존성 주입 
 	private UserRepository userRepository;
+	
+	@GetMapping("/dummy/users")
+	public List<User> list() {
+		return userRepository.findAll();
+	}
+	
+	//한 페이지 당 2건의 데이터 받기 
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size=2, sort="id", direction=Sort.Direction.DESC) Pageable pageable) {
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		
+		List<User> users = pagingUser.getContent();
+		return users;
+	}
 	
 	//{id}주소로 파라미터 전달 받을 수 있음 
 	@GetMapping("/dummy/user/{id}")
