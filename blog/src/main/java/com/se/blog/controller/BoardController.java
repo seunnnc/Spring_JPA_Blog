3a688c09ca010df26f55bc8a6a8f52d754ca3982
seunnnc <seunnnc@users.noreply.github.com>
2020-11-08 03:43:45 +0900

    Spring Securiry 이용하여 login 완료!

--- blog/src/main/java/com/se/blog/controller/BoardController.java
+++ blog/src/main/java/com/se/blog/controller/BoardController.java
@@ -1,6 +1,5 @@
 package com.se.blog.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
